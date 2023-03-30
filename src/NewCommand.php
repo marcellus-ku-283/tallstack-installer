41996e73c502c8b6ea80ee2cc69d2db02842ca9c
Parth Gajjar <parth@selcom.net>
2023-03-30 12:39:53 +0530

    Initial commit for installer

---
+++ src/NewCommand.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace MarcellusKu283\TallstackInstaller;
+
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Create a new Laravel application')
+            ->addArgument('name', InputArgument::REQUIRED);
+            
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        sleep(1);
+
+
+        $commands = [
+            'git clone ' . $this->getRepository(),
+        ];
+
+        if (($process = $this->runCommands($commands, $input, $output))->isSuccessful()) {
+            $output->writeln('  <bg=blue;fg=white> INFO </> Application ready! <options=bold>Build something amazing.</>'.PHP_EOL);
+        }
+
+        return $process->getExitCode();
+    }
+
+    public function getRepository()
+    {
+        return 'https://github.com/marcellus-ku-283/laravel-tallstack-boilerplate.git';
+    }
+
+    /**
+     * Run the given commands.
+     *
+     * @param  array  $commands
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  array  $env
+     * @return \Symfony\Component\Process\Process
+     */
+    protected function runCommands($commands, InputInterface $input, OutputInterface $output, array $env = [])
+    {
+        if (! $output->isDecorated()) {
+            $commands = array_map(function ($value) {
+                if (substr($value, 0, 5) === 'chmod') {
+                    return $value;
+                }
+
+                if (substr($value, 0, 3) === 'git') {
+                    return $value;
+                }
+
+                return $value.' --no-ansi';
+            }, $commands);
+        }
+
+        if ($input->getOption('quiet')) {
+            $commands = array_map(function ($value) {
+                if (substr($value, 0, 5) === 'chmod') {
+                    return $value;
+                }
+
+                if (substr($value, 0, 3) === 'git') {
+                    return $value;
+                }
+
+                return $value.' --quiet';
+            }, $commands);
+        }
+
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, $env, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write('    '.$line);
+        });
+
+        return $process;
+    }
+}
